91d245bbbb61ad41b285368dd50f4f445f2baf64
Aliaksandr Artsikhovich <art-orient@tut.by>
2026-04-24 22:22:40 +0300

    Initial commit

---
+++ src/test/java/com/innowise/paymentservice/InnoPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innowise.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnoPaymentServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
